5074636d36ebc5bf549de5d7d5a4de0839d70e3f
LucaCasa95 <lucacasa95@gmail.com>
2022-04-21 23:52:55 +0200

    spawn animals randomly, colliders

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,21 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
 
-    public GameObject[] randomAnimals;
+    public GameObject[] animals;
+    //spawn range
+    private float spawnRangeX = 20;
+    private float spawnRangeZ = 20;
+    //spawn timer
+    private float startDelay = 2;
+    private float frequencyInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("CreateRandomAnimals", startDelay, frequencyInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void CreateRandomAnimals()
+    {
+        int animalIndex = Random.Range(0, animals.Length);
+        Vector3 spawnXPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+        Instantiate(animals[animalIndex], spawnXPos, animals[animalIndex].transform.rotation);
     }
 }
